887c7d9d4365be6b138b3e9686fb76c2b5bd815d
Bruno Pereyra <brunopereyra459@gmail.com>
2024-05-21 17:20:22 -0300

    Advertencia para mayores.

--- src/player/PlayerMain.tsx
+++ src/player/PlayerMain.tsx
@@ -1,80 +1,63 @@
 import React, { useEffect, useRef, useState } from 'react';
 import flvjs from 'flv.js';
 import Hls from 'hls.js';
-import "./ReactVideoPlayer.css"
+import "./ReactVideoPlayer.css";
 import { AdsAddStreamSummary } from '../services/backGo/streams';
+
 interface ReactVideoPlayerProps {
   src: string;
   videoRef: React.RefObject<HTMLVideoElement>;
   height: string;
   width: string;
-  quality:string;
-  stream:string;
-  streamerDataID:string;
+  quality: string;
+  stream: string;
+  streamerDataID: string;
 }
 
-function ReactVideoPlayer({ src, videoRef, height, width, quality,stream,streamerDataID}: ReactVideoPlayerProps) {
+function ReactVideoPlayer({ src, videoRef, height, width, quality, stream, streamerDataID }: ReactVideoPlayerProps) {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [Commercial, setCommercial] = useState(null);
+  const [Commercial, setCommercial] = useState<any>(null);
+  const [showWarning, setShowWarning] = useState(true); 
+  const [Player, setPlayer] = useState(true); 
   const commercialRef = useRef<HTMLVideoElement | null>(null);
-  const overlayRef = useRef<HTMLDivElement>(null);
   const hlsRef = useRef<Hls | null>(null);
   const pingIntervalRef = useRef<NodeJS.Timeout | null>(null);
   const [socket, setSocket] = useState<WebSocket | null>(null);
 
-
-
   const handleCommercialEnded = async () => {
     setCommercial(null);
     let token = window.localStorage.getItem("token");
-     await AdsAddStreamSummary(token, streamerDataID)
-
+    await AdsAddStreamSummary(token, streamerDataID);
   };
+
   useEffect(() => {
     const REACT_APP_BACKCOMMERCIALWS = process.env.REACT_APP_BACKCOMMERCIALWS;
-    
-    const newSocket = new WebSocket(
-      `wss://www.pinkker.tv/8084/ws/commercialInStream/${stream}`
-    );
+    const newSocket = new WebSocket(`wss://www.pinkker.tv/8084/ws/commercialInStream/${stream}`);
 
-    const connectWebSocket = () => {
-      newSocket.onerror = (error) => {
-        console.error("WebSocket error:", error);
-      };
-      newSocket.onmessage = (event) => {
-        try {
+    newSocket.onerror = (error) => {
+      console.error("WebSocket Error:", error);
+    };
 
-          const receivedMessage = JSON.parse(event.data);
-          if (receivedMessage && receivedMessage.Commercial) {
-            setCommercial(receivedMessage.Commercial);
-            if (commercialRef.current) {
-              commercialRef.current.src = receivedMessage.Commercial;
-              commercialRef.current.play();
-              commercialRef.current.muted = false;
-              
-            }
-          }
-        } catch (error) {
-          console.error("Error al analizar el mensaje JSON:", error);
+    newSocket.onmessage = (event) => {
+      try {
+        const receivedMessage = JSON.parse(event.data);
+        if (receivedMessage && receivedMessage.Commercial) {
+          setCommercial(receivedMessage.Commercial);
+          setShowWarning(false);
         }
-      };
+      } catch (error) {
+        console.error("Error parsing JSON message:", error);
+      }
+    };
 
-      newSocket.onopen = () => {};
-      setSocket(newSocket);
+    newSocket.onopen = () => {};
 
-      window.addEventListener("beforeunload", () => {
-        newSocket.send("closing");
-        newSocket.close();
-      });
-    };   
-     if (!socket || socket.readyState !== WebSocket.OPEN) {
-      connectWebSocket();
-    }
+    window.addEventListener("beforeunload", () => {
+      newSocket.send("close");
+      newSocket.close();
+    });
+
     return () => {
-      window.addEventListener("beforeunload", () => {
-        newSocket.send("closing");
-        newSocket.close();
-      });
       if (newSocket && newSocket.readyState === WebSocket.OPEN) {
         newSocket.close();
       }
@@ -87,42 +70,40 @@ function ReactVideoPlayer({ src, videoRef, height, width, quality,stream,streame
         socket.send("ping");
       }
     };
-  
-    pingInterval(); // Invocar la función aquí para que se ejecute inmediatamente
+
+    pingInterval(); // Call the function here to run it immediately
     const intervalId = setInterval(pingInterval, 3000);
+
     if (pingIntervalRef.current) {
       pingIntervalRef.current = intervalId;
     }
-  
+
     return () => {
       if (pingIntervalRef.current) {
         clearInterval(pingIntervalRef.current);
       }
     };
   }, [socket]);
-  
-  
 
   useEffect(() => {
-
     let flvPlayer: flvjs.Player | null = null;
     let hls: Hls | null = null;
 
     async function initializePlayer() {
-
       try {
         if (flvPlayer) {
-          flvPlayer.pause(); 
+          flvPlayer.pause();
           flvPlayer.unload();
           flvPlayer.detachMediaElement();
           flvPlayer.destroy();
           flvPlayer = null;
         }
-  
+
         if (hls) {
           hls.destroy();
           hls = null;
         }
+
         if (flvjs.isSupported()) {
           flvPlayer = flvjs.createPlayer({
             type: 'flv',
@@ -132,23 +113,21 @@ function ReactVideoPlayer({ src, videoRef, height, width, quality,stream,streame
           if (videoRef.current) {
             flvPlayer.attachMediaElement(videoRef.current);
             flvPlayer.load();
-
             videoRef.current.addEventListener('loadedmetadata', async () => {
               try {
                 await flvPlayer?.play();
                 setIsPlaying(true);
               } catch (error) {
-                console.error('Error reproduciendo el video:', error);
+                console.error('Error playing video:', error);
               }
             });
           }
         } else {
           const mobileInformation = isMobile().toLowerCase();
-
           if (mobileInformation.includes("iphone") || mobileInformation.includes("ipad")) {
             videoRef.current!.src = src + "/index.m3u8";
             videoRef.current!.play().catch(error => {
-              console.error('Error reproduciendo el video:', error);
+              console.error('Error playing video:', error);
             });
           } else {
             hls = new Hls();
@@ -157,7 +136,6 @@ function ReactVideoPlayer({ src, videoRef, height, width, quality,stream,streame
             if (videoRef.current) {
               hls.loadSource(src + "/index.m3u8");
               hls.attachMedia(videoRef.current);
-
               videoRef.current.addEventListener('click', () => {
                 if (!isPlaying) {
                   videoRef.current?.play();
@@ -165,24 +143,21 @@ function ReactVideoPlayer({ src, videoRef, height, width, quality,stream,streame
                 }
               });
               videoRef.current?.play();
-
               hls.on(Hls.Events.MANIFEST_PARSED, () => {
-                console.log('Manifesto analizado');
+                console.log('Manifest parsed');
               });
-
               hls.on(Hls.Events.ERROR, (event, data) => {
-                console.error('Error HLS:', data);
+                console.error('HLS Error:', data);
               });
             }
           }
         }
       } catch (error) {
-        console.error('Error inicializando el reproductor de video:', error);
+        console.error('Error initializing video player:', error);
       }
     }
 
     initializePlayer();
-
     const timeoutId = setTimeout(() => {
       if (!videoRef.current?.readyState || videoRef.current.readyState < 3) {
         initializePlayer();
@@ -191,26 +166,24 @@ function ReactVideoPlayer({ src, videoRef, height, width, quality,stream,streame
 
     return () => {
       clearTimeout(timeoutId);
-    
       if (hls) {
         hls.destroy();
         hls = null;
       }
       if (flvPlayer) {
-        flvPlayer.pause(); 
+        flvPlayer.pause();
         flvPlayer.unload();
         flvPlayer.detachMediaElement();
         flvPlayer.destroy();
         flvPlayer = null;
       }
     };
-    
   }, [src]);
 
   function isMobile() {
     var check = false;
     (function (a) {
-      if (/android|bb\d+|meego|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i) {
+      if (/android|bb\d+|meego|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge|maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)) {
         check = true;
       }
     })(navigator.userAgent || navigator.vendor || "");
@@ -229,40 +202,76 @@ function ReactVideoPlayer({ src, videoRef, height, width, quality,stream,streame
     }
   }
 
-  // Función para desmutear el video
-  const handleUnmuteClick = () => {
+  // Function to handle the click on "Comenzar a mirar"
+  const handleStartWatchingClick = () => {
+    setShowWarning(false);
     if (videoRef.current) {
       videoRef.current.muted = false;
-      if (overlayRef.current) {
-        overlayRef.current.style.display = 'none';
-      }
-    } 
+      setPlayer(false)
+      videoRef.current.play();
+    }
   };
-  
 
   return (
     <>
-      <div
-        ref={overlayRef}
-        className="overlay" 
-        onClick={handleUnmuteClick} 
-        style={{
-          // width: width ,
-          height: height
-        }}
-      >
-        <span
-          className="mute-icon" 
+      {showWarning && (
+        <div
+        style={{ width, height,background:"#080808",paddingTop:"100px", display:"flex",justifyContent:"center"}}
+        
         >
-          🔊 
-        </span>
-      </div>
+        <div
+        className="base-dialog">
+          <div className="dialog-icon-holder">
+            <svg
+             width="70px"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+              className="stroke-white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+              ></path>
+            </svg>
+          </div>
+          <div 
+    
+          className="base-dialogo-Adver"
+       >
+            Advertencia, contenido para mayores de 18 años
+          </div>
+          <div 
+            className="base-dialogo-transmisión"
+         >
+            <p >
+              Esta transmisión tiene contenido para adultos. Debes tener 18 años de edad o más para poder ver este contenido.
+            </p>
+          </div>
+          <div  className="dialog-actions">
+              <button >
+                <div >
+                  <div>Cancelar</div>
+                </div>
+              </button>
+              <button
+                onClick={handleStartWatchingClick}
+              >
+                <div >
+                  <div >Comenzar a mirar</div>
+                </div>
+              </button>
+          </div>
+        </div>
+        </div>
+      )}
       {Commercial && (
         <video
-          style={{
-            width: width,
-            height: height,
-          }}
+          style={{ width, height }}
           id='commercial-player'
           muted={true}
           controls={false}
@@ -272,11 +281,8 @@ function ReactVideoPlayer({ src, videoRef, height, width, quality,stream,streame
         />
       )}
       <video
-        style={{
-          width: width,
-          height: height,
-          display: Commercial ? "none":""
-        }}
+        style={{ width, height, display: Commercial || Player ? "none" : "" }}
+
         id='video-player'
         muted={true}
         controls={false}
