4b0cf858cdde6a9d20919713e88b7df128097d7e
Bruno Pereyra <brunopereyra459@gmail.com>
2024-07-08 20:39:14 -0300

    clips recomendados

--- src/components/clips/main/ClipsMain.jsx
+++ src/components/clips/main/ClipsMain.jsx
@@ -67,9 +67,14 @@ export default function ClipsMain({ tyExpanded, expandedLeft }) {
           res = await GetClipsCategory("", 1, "");
         }
 
-        if (res.data.message === "ok") {
+        if (res.data.message === "ok" && res.data.data != null) {
           const newClips = res.data.data;
           setClips([...clips, ...newClips]);
+        } else {
+          res = await GetClipsMostViewed(1);
+          if (res.data.message === "ok") {
+            setClips([...clips, ...res.data.data]);
+          }
         }
       } catch (error) {
         console.error(error);
